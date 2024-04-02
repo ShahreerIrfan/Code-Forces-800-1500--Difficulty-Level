8c87d254afd6bb07b528ebf082012ca71b4f0b10
Your Name <mdshahreerirfan@gmail.com>
2024-04-02 22:27:02 +0600

    Prime_Factorization added

---
+++ Python/max_min.py
@@ -0,0 +1,11 @@
+# max min
+nums = []
+a,b,c,d,e = input().split()
+nums.append(a)
+nums.append(b)
+nums.append(c)
+nums.append(d)
+nums.append(e)
+print(max(nums))
+print(min(nums))
+ 
---
+++ Python/sum_two_float.py
@@ -0,0 +1,7 @@
+def sum_two_float(x, y):
+    result = float(x) + float(y)
+    print(result)
+
+a, b = input().split()  
+sum_two_float(a, b)
+
