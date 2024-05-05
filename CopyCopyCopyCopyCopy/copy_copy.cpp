094ffbf19348d330e8360b92b71d7146f7162a30
Your Name <mdshahreerirfan@gmail.com>
2024-05-06 01:27:25 +0600

    [Added] copy_copy.cpp

---
+++ CopyCopyCopyCopyCopy/copy_copy.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+void solve(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++) cin>>arr[i];
+    set<int>s;
+    for(int i=0;i<n;i++) s.insert(arr[i]);
+    cout<<s.size()<<endl;
+
+}
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        solve();
+    }
+}
